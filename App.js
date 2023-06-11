3d8031e733e111b8a9371fd994f58115b40527b3
drkspark <madhusudan753@gmail.com>
2023-06-11 16:20:35 +0530

    Functional Components

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1 id='heading'>Hello JSX</h1>;
+// React Component
+// Class Based Components - OLD
+// Functional Based Components - NEW
 
-// If we write JSX in multiple lines, then we have to enclose it in () paranthesis
-const divHeading = (
-    <div id='home2' className='homiie'>
-        <h2>Hello Hommie</h2>
+// React Functional Component is just a JS function which return JSX
+// Always start name of a Component with capital alphabet
+
+// This is a React Element
+const heading = (
+    <h1 className='head' tabIndex='5'>
+        Namaste React using JSX
+    </h1>
+);
+
+// These are React Components
+//? Return is optional when we make components using Arrow Functions
+
+function Heading() {
+    // return is Mandatory
+    return <h1 id='heading'>Hello Functional Component</h1>;
+}
+
+//? Component Composition is Nesting of Components 
+const Healer = () => (
+    <div className='container'>
+        <Heading />
+        <h1 id='heading'>Rage Spell</h1>
     </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//! To render a component we need to enclose it inside <ComponentName />
 
-root.render(jsxHeading);
+root.render(<Healer />);
+//? root.render(heading);  // This is a React Element
