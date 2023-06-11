de0473ae17af4925735e56a9ea8114f0fd85d9c4
drkspark <madhusudan753@gmail.com>
2023-06-11 11:11:58 +0530

    Created Sibling Elements in React

--- App.js
+++ App.js
@@ -9,16 +9,21 @@
  *
  */
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
     React.createElement(
-        // This is the place for the Children i.e we
+        // This is the place for the Children i.e if we want more than 1 child, then we can pass a Array
         "div",
         { id: "child" },
-        React.createElement("h1", {}, "I am a H1 Tag inside Div's")
-    )
-); // {} : Attribute to the tag
+        [
+            React.createElement("h1", {}, "I am a H1 Tag inside Div's"),
+            React.createElement("h2", {}, "I am a H2 Tag inside Div's"),
+        ]
+    ),
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am a H1 Tag inside Div's"),
+        React.createElement("h2", {}, "I am a H2 Tag inside Div's"),
+    ]),
+]); // {} : Attribute to the tag
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
