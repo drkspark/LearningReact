d80a0a205fd190d705b09c926e46a67869099294
drkspark <madhusudan753@gmail.com>
2023-06-11 15:04:19 +0530

    Learned about Parcel, BrowserList

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
-/**
- * How to create nested elements ?
- *
- * <div id="parent">
- *      <div id="child">
- *          <h1>I am a H1 Tag inside Div's</h1>
- *      </div>
- * </div>
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement(
-        // This is the place for the Children i.e if we want more than 1 child, then we can pass a Array
-        "div",
-        { id: "child" },
-        [
-            React.createElement("h1", {}, "I am a H1 Tag inside Div's"),
-            React.createElement("h2", {}, "I am a H2 Tag inside Div's"),
-        ]
-    ),
     React.createElement("div", { id: "child" }, [
         React.createElement("h1", {}, "I am a H1 Tag inside Div's"),
         React.createElement("h2", {}, "I am a H2 Tag inside Div's"),
     ]),
-]); // {} : Attribute to the tag
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am a H1 Tag inside Div's"),
+        React.createElement("h2", {}, "I am a H2 Tag inside Div's"),
+    ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
