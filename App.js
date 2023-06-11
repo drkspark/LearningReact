4d17ffcaa3cf053d974c32371e7cd66bb0c36cb9
drkspark <madhusudan753@gmail.com>
2023-06-11 15:22:34 +0530

    Created using functions

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I am a H1 Tag inside Div's"),
-        React.createElement("h2", {}, "I am a H2 Tag inside Div's"),
-    ]),
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I am a H1 Tag inside Div's"),
-        React.createElement("h2", {}, "I am a H2 Tag inside Div's"),
-    ]),
-]);
+// React Element
+// React.createElement => object => HTMLElement(render)
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello React");
+
+console.log(heading); // Gives the Object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
