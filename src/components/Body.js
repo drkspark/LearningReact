32cb044a849ff9d0b2a123e7ad9f0d97c422e0bf
drkspark <madhusudan753@gmail.com>
2023-06-17 16:47:30 +0530

    Added new constant Data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,17 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
+
+
+
+
 const Body = () => {
     return (
         <div className='body'>
             <div className='Search'>Search</div>
             <div className='res-container'>
                 {resList.map((restraunt) => (
-                    <RestaurantCard key={restraunt.id} resData={restraunt} />
+                    <RestaurantCard key={restraunt.data.id} resData={restraunt.data} />
                 ))}
             </div>
         </div>
