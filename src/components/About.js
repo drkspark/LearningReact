4f22286fc7bf98731800ca2d3b97d861f998d03c
drkspark <madhusudan753@gmail.com>
2023-06-18 11:34:08 +0530

    Added Basic routes and ErrorRoute

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>This is my About Page</h1>
+            <h2>I am Learning React JS from Namaste React</h2>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Hello, My name is Drkspark and this is my App</h1>
+            <h2>You can find everywhere with username: drkspark</h2>
+        </div>
+    );
+}
+
+export default Contact;
