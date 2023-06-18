612266982a700786dd29c31716bdbc063fda96c3
drkspark <madhusudan753@gmail.com>
2023-06-18 10:16:16 +0530

    Completed exploring the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,8 +3,12 @@ import Shimmer from "./Shimmer";
 import { useState, useEffect } from "react";
 
 const Body = () => {
+    const [apiData, setApiData] = useState([]);
     const [restaurantData, setRestarauntData] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
+    console.log("Body Rendered"); // This helps us visualise that our body is being re-rendered again whenever we type
+    
     useEffect(() => {
         fetchData();
     }, []);
@@ -15,35 +19,63 @@ const Body = () => {
         );
 
         const json = await data.json();
+        setApiData(json?.data?.cards[2]?.data?.data?.cards);
         setRestarauntData(json?.data?.cards[2]?.data?.data?.cards);
     };
-
+    /**
+     * To use the Input text-box we need to do 2 things:
+     * 1. Bind the text-box with a local State Variable
+     * If we only do step1, we won't be able to see the text being typed in it.
+     * 2. Add onChange which will update the, text being typed into the input box
+     *
+     * ! Every time we type a Character, it re-renders the whole body, and just changes the Text in input box. That's so effecient
+     *
+     * ! Whenever a state variable update, react triggers a reconciliation cycle (re-renders the component)
+     */
     return restaurantData.length === 0 ? (
         <Shimmer />
     ) : (
         <div className='body'>
-            <button
-                className='top-rated'
-                onClick={() => {
-                    const resData = resList.filter(
-                        (restaurant) => restaurant.data.avgRating > 4.0
-                    );
-                    setRestarauntData(resData);
-                }}
-            >
-                Top Rated Restraunts
-            </button>
-            <button
-                className='low-rated'
-                onClick={() => {
-                    const resData = resList.filter(
-                        (restaurant) => restaurant.data.avgRating < 4.0
-                    );
-                    setRestarauntData(resData);
-                }}
-            >
-                Low Rated Restraunts
-            </button>
+            <div className='filter'>
+                <div className='search'>
+                    <input
+                        type='text'
+                        className='search-box'
+                        value={searchText}
+                        onChange={(e) => {
+                            setSearchText(e.target.value);
+                        }}
+                    />
+                    <button
+                        onClick={() => {
+                            // Filter the cards and update UI
+                            const searchedRestraunt = apiData.filter(
+                                (restaurant) =>
+                                    restaurant.data.name
+                                        .toLowerCase()
+                                        .includes(searchText.toLowerCase())
+                            );
+
+                            setRestarauntData(searchedRestraunt);
+                        }}
+                    >
+                        Search
+                    </button>
+                </div>
+
+                <button
+                    className='top-rated'
+                    onClick={() => {
+                        const resData = resList.filter(
+                            (restaurant) => restaurant.data.avgRating > 4.0
+                        );
+                        setRestarauntData(resData);
+                    }}
+                >
+                    Top Rated Restraunts
+                </button>
+            </div>
+
             <div className='res-container'>
                 {restaurantData.map((restraunt) => (
                     <RestaurantCard
